c4cd0f7d9bafbc08ad644c0756e33998cb01948d
chedahub <81345302+chedahub@users.noreply.github.com>
2023-08-07 15:11:01 +0900

    Update pwnset_22.sh

--- pwnset_22.sh
+++ pwnset_22.sh
@@ -59,7 +59,7 @@ sudo cp ~/.cargo/bin/pwninit /usr/local/bin/
 
 # visual studio code
 sudo apt-get install curl -y
-sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo sh -c 'curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > /etc/apt/trusted.gpg.d/microsoft.gpg'
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 sudo apt update
 sudo apt install code -y
